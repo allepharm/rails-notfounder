b2abcbbe0aa97e53fd7c87de77243af57c454ed6
Aleksander Długopolski <aleksander.dlugopolski@logihub.pl>
2019-11-14 20:34:41 +0100

    Fix missing block end

--- app/mailers/rails_not_founder/request_log_mailer.rb
+++ app/mailers/rails_not_founder/request_log_mailer.rb
@@ -1,12 +1,13 @@
 module RailsNotFounder
   class RequestLogMailer < ApplicationMailer
-  layout false
-  default from: RailsNotFounder.configuration.sender_email,
-          to: RailsNotFounder.configuration.receiver_email
+    layout false
+    default from: RailsNotFounder.configuration.sender_email,
+            to: RailsNotFounder.configuration.receiver_email
 
-  def notify(log, subject)
-    @log = log
-    @subject = subject
-    mail(subject: subject)
+    def notify(log, subject)
+      @log = log
+      @subject = subject
+      mail(subject: subject)
+    end
   end
 end
